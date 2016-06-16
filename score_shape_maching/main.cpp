d6b4c09969aa3f2ef24b47b5595f15d476df9d2b
bassaer <rf.1799@gmail.com>
2016-06-16 15:41:39 +0900

    Initial Commit

---
+++ score_shape_maching/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  score_shape_maching
+//
+//  Created by Nakayama on 2016/06/16.
+//  Copyright © 2016年 Nakayama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
